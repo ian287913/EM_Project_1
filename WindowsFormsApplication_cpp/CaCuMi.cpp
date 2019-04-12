2feddfb2c778a03aee221ff0f5049946de553a7d
ian287913 <45512766+ian287913@users.noreply.github.com>
2019-04-12 16:31:19 +0800

    Cofactor and Determinants

--- WindowsFormsApplication_cpp/CaCuMi.cpp
+++ WindowsFormsApplication_cpp/CaCuMi.cpp
@@ -4,7 +4,7 @@ const Matrix CaCuMi::Multiply(const Matrix & M1, const Matrix & M2)
 {
 	if (M1.Data[0].size() != M2.Data.size())
 		throw std::exception("(Multiply) cannot match the size.");
-	
+
 	int row = M1.Data.size(), col = M2.Data[0].size();
 	int depth = M2.Data.size();
 	Matrix tempMatrix = Matrix(row, col);
@@ -44,6 +44,12 @@ const Matrix CaCuMi::Multiply(const Matrix & M, const double & scale)
 const double CaCuMi::Determinant(const Matrix & M)
 {
 	int row = M.Data.size(), col = M.Data[0].size();
+	//	Zero
+	if (row == 0 || col == 0)
+		throw std::exception("(Determinant) 0-dim unacceptable");
+	//	Vector-like
+	if (row == 1 || col == 1)
+		return 0.0;
 	double value = 0.0;
 	double add = 1.0;
 	double sub = -1.0;
@@ -63,22 +69,34 @@ const double CaCuMi::Determinant(const Matrix & M)
 	return value;
 }
 
-///...
 const Matrix CaCuMi::Cofactors(const Matrix & M)
 {
-	Matrix tempMatrix = Matrix(M);
 	int row = M.Data.size(), col = M.Data[0].size();
+	//	Zero
+	if (row < 2 || col <2)
+		throw std::exception("(Cofactors) 0or1 dim unacceptable");
+
+	Matrix tempMatrix = Matrix(M);
 
 	for (int r = 0; r < row; r++)
 	{
 		for (int c = 0; c < col; c++)
 		{
-			///...
-			///
+			Matrix determinTemp = Matrix(M);
+			//	cut	a row
+			determinTemp.Data.erase(determinTemp.Data.begin() + c);
+			//	cut	a column
+			for (int cut = 0; cut < determinTemp.Data.size(); cut++)
+				determinTemp.Data[cut].erase(determinTemp.Data[cut].begin() + r);
+			//	do Determinant
+			tempMatrix.Data[r][c] = Determinant(determinTemp);
+			//	1 or -1
+			if (((r + c) % 2) != 0)
+				tempMatrix.Data[r][c] *= (-1.0);
 		}
 	}
 
-	return Matrix();
+	return tempMatrix;
 }
 
 void CaCuMi::ZeroCheck(const Matrix & M)
